defe123acc914bb430da0414ef0ba8b3b5df1e09
Nallely Garcia <98186591+nallely64@users.noreply.github.com>
2022-03-02 21:58:58 -0600

    Advance: 191/281, 67%

--- koans/about_scoring_project.rb
+++ koans/about_scoring_project.rb
@@ -28,11 +28,40 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # More scoring examples are given in the tests below:
 #
 # Your goal is to write the score method.
+def handle_ones(found)
+  three_ones, other_ones = found.divmod 3
+  1000 * three_ones + 100 * other_ones
+end
+
+def handle_fives(found)
+  three_fives, other_fives = found.divmod 3
+  500 * three_fives + 50 * other_fives
+end
 
 def score(dice)
   # You need to write this method
+  points = 0
+  counts = {}
+
+  dice.uniq.each do |value|
+    counts[value] = dice.count value
+  end
+
+  counts.each do |value, found|
+    points += case value
+    when 1
+      handle_ones found
+    when 5
+      handle_fives found
+    else
+      three_nums, = found.divmod 3
+      value * 100 * three_nums
+    end
+  end
+  points
 end
 
+# class AboutScoringProject
 class AboutScoringProject < Neo::Koan
   def test_score_of_an_empty_list_is_zero
     assert_equal 0, score([])
@@ -73,5 +102,4 @@ class AboutScoringProject < Neo::Koan
     assert_equal 1200, score([1,1,1,1,1])
     assert_equal 1150, score([1,1,1,5,1])
   end
-
 end
